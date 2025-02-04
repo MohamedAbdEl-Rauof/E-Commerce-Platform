8391547b198c38a68c441b388c059d0488be5247
mohamedabdelraouf <you@example.com>
2025-02-04 11:45:29 +0200

    try to solve the id of category problem

--- next.config.ts
+++ next.config.ts
@@ -1,8 +1,43 @@
-import type {NextConfig} from "next";
+import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     images: {
         domains: ['images.pexels.com'],
+    },
+    reactStrictMode: true,
+    experimental: {
+        appDir: true,
+    },
+    swcMinify: true,
+    // Add custom headers
+    async headers() {
+        return [
+            {
+                source: '/(.*)',
+                headers: [
+                    {
+                        key: 'X-Frame-Options',
+                        value: 'DENY',
+                    },
+                    {
+                        key: 'X-Content-Type-Options',
+                        value: 'nosniff',
+                    },
+                    {
+                        key: 'Referrer-Policy',
+                        value: 'strict-origin-when-cross-origin',
+                    },
+                ],
+            },
+        ];
+    },
+    async rewrites() {
+        return [
+            {
+                source: '/user/categories/:categoryName/:productName',
+                destination: '/user/categories/[categoryName]/[productName]',
+            },
+        ];
     },
 };
 
