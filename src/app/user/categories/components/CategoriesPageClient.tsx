db47f847bfd806c4cfa3005a30deb12e97313315
mohamedabdelraouf <mohamedabdelraouf>
2025-02-08 11:22:40 +0200

    edit category page

---
+++ src/app/user/categories/components/CategoriesPageClient.tsx
@@ -0,0 +1,25 @@
+"use client";
+import React from 'react';
+import {Container, Typography} from '@mui/material';
+import {useCategories} from '@/context/CategoriesContext';
+import CategoryBanner from './CategoryBanner';
+import CategoryGrid from './CategoryGrid';
+
+const CategoriesPageClient = () => {
+    const {categories, loading} = useCategories();
+
+    return (
+        <div className="min-h-screen">
+            <Container maxWidth={false} sx={{maxWidth: '2000px'}}>
+                <CategoryBanner loading={loading}/>
+                <Typography variant="h4" component="h5" align="center" gutterBottom
+                            sx={{marginTop: '2.5rem', marginBottom: '4rem'}}>
+                    Explore Our Categories
+                </Typography>
+                <CategoryGrid categories={categories} loading={loading}/>
+            </Container>
+        </div>
+    );
+};
+
+export default CategoriesPageClient;
--- src/app/user/categories/page.tsx
+++ src/app/user/categories/page.tsx
@@ -1,27 +1,5 @@
-"use client";
-import React from 'react';
-import {Container, Typography} from '@mui/material';
-import {useCategories} from '@/context/CategoriesContext';
-import CategoryBanner from './components/CategoryBanner';
-import CategoryGrid from './components/CategoryGrid';
+import CategoriesPageClient from './components/CategoriesPageClient';
 
-const CategoriesPage = () => {
-    const {categories, loading} = useCategories();
-
-    return (
-        <>
-            <div className="min-h-screen">
-                <Container maxWidth={false} sx={{maxWidth: '2000px'}}>
-                    <CategoryBanner loading={loading}/>
-                    <Typography variant="h4" component="h5" align="center" gutterBottom
-                                sx={{marginTop: '2.5rem', marginBottom: '4rem'}}>
-                        Explore Our Categories
-                    </Typography>
-                    <CategoryGrid categories={categories} loading={loading}/>
-                </Container>
-            </div>
-        </>
-    );
-};
-
-export default CategoriesPage;
+export default function CategoriesPage() {
+    return <CategoriesPageClient/>;
+}
